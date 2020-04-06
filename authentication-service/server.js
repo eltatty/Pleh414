314535cb44495c014d843a48cd87206a2975a22a
eltatty <49652438+eltatty@users.noreply.github.com>
2020-04-06 12:29:11 +0300

    Delete server.js

--- authentication-service/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./src/app')
-
-const port = process.env.PORT || 3001
-
-app.listen(port, () => {
-    console.log('Listening on port ' + port)
-    console.log('-------------------------')
-})
